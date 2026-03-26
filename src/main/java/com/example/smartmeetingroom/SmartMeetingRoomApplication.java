313edb1a9ffe5291fe0903d4358d3ea1b01bbd22
Melwin <melwinmanish30@gmail.com>
2026-03-26 11:25:05 +0530

    initial commit

---
+++ src/main/java/com/example/smartmeetingroom/SmartMeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmeetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeetingRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMeetingRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartmeetingroom/SmartMeetingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartmeetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeetingRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
